7dd405cc18f24f9cd8a8ce335ba6cb3aea49778a
Omar Sharieff <omarsharief642002@gmail.com>
2024-09-15 13:04:32 +0530

    Controllers and Router

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,3 +15,9 @@ app.use(express.urlencoded({limit: "16kb", extended: true}));
 app.use(express.json({extended: true}));
 app.use(express.static('public'));
 app.use(cookieParser());
+
+//Import Router
+import userRouter from "./routes/user.routes.js";
+
+//Router declaration
+app.use("/user", userRouter);
